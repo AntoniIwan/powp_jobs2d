78df2065542f29a1cb5fd37bf44ed2a48f9b25f4
Masloorzech <antoniiwan02@gmail.com>
2025-03-20 12:53:47 +0100

    Feature: Added one interface and two inheritance Classes, deleted inheritance from DriverCommand

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -1,18 +1,20 @@
 package edu.kis.powp.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class OperateToCommand implements DriverCommand{
+  private int x;
+  private int y;
+  private Job2dDriver driver;
+
+  public OperateToCommand(int x, int y, Job2dDriver driver) {
+    this.x = x;
+    this.y = y;
+    this.driver = driver;
+  }
+
   @Override
   public void execute() {
-
-  }
-
-  @Override
-  public void setPosition(int i, int i1) {
-
-  }
-
-  @Override
-  public void operateTo(int i, int i1) {
-
+    driver.operateTo(x, y);
   }
 }
