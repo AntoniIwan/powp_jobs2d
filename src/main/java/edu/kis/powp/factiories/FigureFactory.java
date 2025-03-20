294475834a72a75dbfff4f6b072e3a374813dee1
Masloorzech <antoniiwan02@gmail.com>
2025-03-20 13:48:29 +0100

    Feature: Added FigureFactory and static values for tests

--- src/main/java/edu/kis/powp/factiories/FigureFactory.java
+++ src/main/java/edu/kis/powp/factiories/FigureFactory.java
@@ -1,15 +1,41 @@
 package edu.kis.powp.factiories;
 import edu.kis.powp.command.ComplexCommand;
+import edu.kis.powp.command.OperateToCommand;
+import edu.kis.powp.command.SetPositionCommand;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+import edu.kis.powp.jobs2d.drivers.adapter.Jobs2DDriverToAbstractDriverAdapter;
 
 public class FigureFactory {
-  private DriverManager driverManager;
-  private int x;
+  private  DriverManager driverManager;
+  private  int x;
   private int y;
-  private FigureFactory(int x, int y, DriverManager driverManager) {
+  public FigureFactory(int x, int y, DriverManager driverManager) {
     this.x = x;
     this.y = y;
     this.driverManager = driverManager;
+  }
+
+  public ComplexCommand generateRectangleCommand(int width, int height){
+    ComplexCommand command = new ComplexCommand();
+    Job2dDriver main_driver = driverManager.getCurrentDriver();
+    command.addCommand(new SetPositionCommand(x,y,main_driver));
+    command.addCommand(new OperateToCommand(x+width,y,main_driver));
+    command.addCommand(new OperateToCommand(x+width,y+height,main_driver));
+    command.addCommand(new OperateToCommand(x,y+height,main_driver));
+    command.addCommand(new OperateToCommand(x,y,main_driver));
+    return command;
+  }
+
+  public ComplexCommand generateTriangleCommand(int radius){
+    ComplexCommand command = new ComplexCommand();
+    Job2dDriver main_driver = driverManager.getCurrentDriver();
+    command.addCommand(new SetPositionCommand(x,y,main_driver));
+    command.addCommand(new OperateToCommand(x+radius,y,main_driver));
+    command.addCommand(new OperateToCommand(x,y+radius,main_driver));
+    command.addCommand(new OperateToCommand(x,y,main_driver));
+    return command;
   }
 
 }
--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -3,10 +3,13 @@ package edu.kis.powp.jobs2d.events;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import edu.kis.powp.command.ComplexCommand;
+import edu.kis.powp.factiories.FigureFactory;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.drivers.adapter.Jobs2DDriverToAbstractDriverAdapter;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJane;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
+import edu.kis.powp.jobs2d.TestJobs2dPatterns;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
@@ -19,15 +22,25 @@ public class SelectTestFigureOptionListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
     switch (e.getActionCommand()) {
-      case "Figure Joe 1":
-          FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-          break;
-        case "Figure Joe 2":
+      case TestJobs2dPatterns.FIGURE_JOE_1:
+        FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+        break;
+      case TestJobs2dPatterns.FIGURE_JOE_2:
           FiguresJoe.figureScript2(driverManager.getCurrentDriver());
           break;
-          case "Figure Jane":
+      case TestJobs2dPatterns.FIGURE_JANE:
             Jobs2DDriverToAbstractDriverAdapter temp = new Jobs2DDriverToAbstractDriverAdapter(driverManager.getCurrentDriver());
             FiguresJane.figureScript(temp);
+      case TestJobs2dPatterns.RECTANGLE:
+        FigureFactory rectangleFigure = new FigureFactory(0, 0, driverManager);
+            ComplexCommand commandFromFactory = rectangleFigure.generateRectangleCommand(150,120);
+            commandFromFactory.execute();
+          break;
+      case TestJobs2dPatterns.TRIANGLE:
+        FigureFactory shape = new FigureFactory(0,0,driverManager);
+        ComplexCommand command = shape.generateTriangleCommand(150);
+        command.execute();
+        break;
         default:
           break;
     }
